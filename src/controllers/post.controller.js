b5063e5c1e9c452cbe56bb8ecc6a3790f250ea1a
Aman Singh <amanscode2005@gmail.com>
2026-02-11 22:30:16 +0530

    integerate imagekit for storage

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,5 +1,7 @@
 const postModel = require("../models/post.model")
-const generateCaption = require("../services/ai.services")
+const generateCaption = require("../services/ai.services");
+const uploadFile = require("../services/storage.service");
+const {v4:uuidv4} = require("uuid")
 
 async function createPostController  (req, res){
     const  file = req.file;
@@ -8,8 +10,17 @@ async function createPostController  (req, res){
 
     const caption = await generateCaption(base64Image);
 
-    res.json({
-        caption
+    const result = await uploadFile(file.buffer, `${uuidv4()}`)
+
+    const post = await postModel.create({
+        image:result.url,
+        caption:caption,
+        user:req.user._id
+    })
+
+    return res.status(201).json({
+        message:"Post created successfully",
+        post
     })
 }
 
